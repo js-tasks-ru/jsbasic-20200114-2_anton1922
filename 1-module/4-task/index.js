ba811e768427cdf7e5c8d111c4e38c6af1e5f4ea
Anton Fedorov <anton.pzkt@gmail.com>
2020-01-19 22:58:13 +0100

    completed 1-module/4-task

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -3,6 +3,19 @@
  * @param {string} str base
  * @returns {boolean}
  */
+'use strict';
+
 function checkSpam(str) {
 
+  if (str === null) {
+    return false;
+  }
+
+  str = str.toLowerCase();
+
+  if (str.indexOf('1xbet') != -1 || str.indexOf('xxx') != -1) {
+    return true;
+  }
+
+  return false;
 }
