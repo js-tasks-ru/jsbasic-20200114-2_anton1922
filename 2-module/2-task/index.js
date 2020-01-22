6733c0f3d6dc98da97c37bc00550c84d0fcb1f5c
Anton Fedorov <anton.pzkt@gmail.com>
2020-01-23 00:15:44 +0100

    completed 2-module/2-task

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -3,5 +3,18 @@
  * @param {Object} obj
  * @returns {Boolean}
  */
+'use strict';
+
 function isEmpty(obj) {
+  let i = 0;
+
+  for (let prop in obj) {
+    i++;
+  }
+
+  if (i == 0) {
+    return true;
+  }
+
+  return false;
 }
