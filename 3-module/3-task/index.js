4c68e9f830d66dcb8a346f99ced6bd3505974c6f
Anton Fedorov <anton.pzkt@gmail.com>
2020-01-31 01:01:12 +0100

    completed 3-module/3-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -2,5 +2,30 @@
  * @param {string} str
  * @returns {string}
  */
+
+'use strict';
+let string = 'background-color';
+
 function camelize(str) {
+  //transform string to array
+  let primaryArray = str.split('');
+  //create new array
+  let modifiedArray = [];
+
+  //add an element to the beginning of the primaryArray
+  primaryArray.unshift(' ');
+
+  //transform symbols after '-' to upper case and add in modifiedArray
+  primaryArray.reduce((prevValue, curValue, index, array) => {
+    modifiedArray[index] = (curValue === '-') ? primaryArray[index + 1].toUpperCase() :
+      primaryArray[index + 1];
+  }, '');
+
+  //cut off the last element of the modifiedArray
+  modifiedArray.splice(modifiedArray.length - 1);
+
+  //remove '-' from the modifiedArray and return final string
+  return modifiedArray.filter((item) => {
+    return item !== '-';
+  }).join('');
 }
