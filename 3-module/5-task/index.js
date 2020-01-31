639546129efcac32b9fde8df331f21550cd6ed55
Anton Fedorov <anton.pzkt@gmail.com>
2020-01-31 01:48:52 +0100

    completed 3-module/5-task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,12 @@
  * @param {number} b
  * @returns {number[]}
  */
+
+'use strict';
+
 function filterRange(arr, a, b) {
+  //filter array and return filtered result
+  return arr.filter(item => {
+    return item >= a && item <= b;
+  });
 }
