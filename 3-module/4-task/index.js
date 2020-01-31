9187d76ad16dd3037dd5d352ea0b1fc0883e091b
Anton Fedorov <anton.pzkt@gmail.com>
2020-01-31 01:27:03 +0100

    completed 3-module/4-task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -2,5 +2,12 @@
  * @param   {{ name: string, age: number }[]} users
  * @returns {string[]}  объект
  */
+
+'use strict';
+
 function namify(users) {
+  //extract property 'name' from each object, return as item of array and return result
+  return users.map(item => {
+    return item.name;
+  });
 }
