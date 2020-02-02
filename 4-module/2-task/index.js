c93d72ffae1292d13d3e70911713e53bb379e6ca
Anton Fedorov <anton.pzkt@gmail.com>
2020-02-02 23:16:21 +0100

    completed 4-module/2-task

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -2,5 +2,22 @@
  * @param {HTMLTableElement} table
  * @return {void}
  */
+
+'use strict';
+
 function makeDiagonalRed(table) {
+  //set and reset a counter
+  let i = 0;
+
+  //iterate a collection rows of 'tbody'
+  for (let tr of table.querySelector('tbody').rows) {
+    
+    //in each 'tr-row' get a collection 'td-cells' and 
+    //change a background color of a 'td-cell' with index 'i' to red
+    tr.cells[i].style.backgroundColor = 'red';
+
+    i++;
+  }
+
+  return table;
 }
