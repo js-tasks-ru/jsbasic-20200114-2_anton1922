28a3ea3852cca948d3e2ed76af61a47cf55f3ed7
Anton Fedorov <anton.pzkt@gmail.com>
2020-02-13 00:23:40 +0100

    completed 6-module/1-task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -16,7 +16,53 @@
 class ClearedTable {
   constructor(data) {
     this.el = document.createElement('table');
+    this.el.classList.add('pure-table');
+
     this.data = data;
+
+    const templateTableHead = `
+    <thead>
+    <tr>
+    <td>Name</td>
+    <td>Age</td>
+    <td>Salary</td>
+    <td>City</td>
+    <td></td>
+    </tr>
+    </thead>`;
+
+    this.el.insertAdjacentHTML('beforeend', templateTableHead);
+
+    this.tableBody = document.createElement('tbody');
+
+    for (let row of this.data) {
+      const templateRow = `
+      <tr data-id=${row.id}>
+      <td>${row.name}</td>
+      <td>${row.age}</td>
+      <td>${row.salary}</td>
+      <td>${row.city}</td>
+      <td><a href="#delete">X</a></td>
+      </tr>`;
+
+      this.tableBody.insertAdjacentHTML('beforeend', templateRow);
+    }
+
+    this.el.appendChild(this.tableBody);
+
+    this.el.addEventListener('click', (event) => {
+      if (event.target.tagName == 'A') {
+        this.removeRow();
+      }
+    });
+  }
+
+  removeRow() {
+    const row = event.target.closest('tr');
+
+    row.remove();
+
+    this.onRemoved(Number(row.dataset.id));
   }
 
   /**
@@ -24,6 +70,7 @@ class ClearedTable {
    * @param {number} id - идентификатор удаляемого пользователя
    */
   onRemoved(id) {}
+
 }
 
 window.ClearedTable = ClearedTable;
